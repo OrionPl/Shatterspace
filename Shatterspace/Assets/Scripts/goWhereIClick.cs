17e7b53600664254c1120f8262780da8300401d8
ccegeadamcc <egemenkazanasmaz@gmail.com>
2018-06-04 20:23:38 +0300

    Update of squad AI
    - Added placeholders to prefab
    - Created a 'man' for testing purposes
    - Changed Camera's position for better view.

---
+++ Shatterspace/Assets/Scripts/goWhereIClick.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goWhereIClick : MonoBehaviour {
+
+    private UnityEngine.AI.NavMeshAgent aIController;
+
+    // one time run
+    void Start()
+    {
+        //Declare a variable for navmesh componnent
+        aIController = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    //that will called every frame
+    void Update()
+    {
+        // if we click anywhere on screen with right mouse button
+        if (Input.GetMouseButtonDown(1))
+        {
+            // send ray
+            RaycastHit hit;
+            Ray clickRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            // if raycast hit  to an object
+            if (Physics.Raycast(clickRay, out hit))
+            {
+                // set hit.point as target
+                aIController.destination = hit.point;
+            }
+        }
+    }
+
+}
