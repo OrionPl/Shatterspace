84f493d230a6d6cfd2177e821941e137132dd236
Mikołaj Kierski <mikolaj.kierski@outlook.com>
2018-06-06 23:01:48 +0200

    Added vertical camera rotation

---
+++ Outback Island/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+    public float mouseSensitivity = 1;
+    public float movementSpeed;
+    public float jumpForce;
+    public float runMultiplier;
+
+    private Rigidbody rb;
+
+    private bool isGrounded = true;
+
+
+
+
+    /////////////////////////////////////
+    private Transform cam;
+
+    public float ammo;
+    public float range;
+    public float fireRate;
+    public float impactForce;
+
+    private float nextTimeToFire = 0;
+    /////////////////////////////////////
+
+    /////////////////////////////////////
+    private Quaternion playerTargetRotation;
+    private Quaternion cameraTargetRotation;
+
+
+    /////////////////////////////////////
+
+
+    void Start () {
+        /////////////////////////////////////
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        rb = GetComponent<Rigidbody>();
+        cam = transform.GetChild(0);
+        /////////////////////////////////////
+
+        /////////////////////////////////////
+        playerTargetRotation = transform.localRotation;
+        cameraTargetRotation = cam.localRotation;
+    }
+
+    void Update () {
+        GroundCheck();
+
+
+
+
+
+        /////////////////////////////////////
+        float moveX = 0;
+        float moveY = 0;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveX += 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveX -= 1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveY += 1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveY -= 1;
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+            moveX = moveX * runMultiplier;
+        /////////////////////////////////////
+
+
+
+        /////////////////////////////////////
+        Vector3 x = transform.forward * moveX;
+        Vector3 y = transform.right * moveY;
+        rb.AddForce((x + y) * movementSpeed);
+
+
+        /////////////////////////////////////
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true)
+            rb.AddForce(0, jumpForce, 0);
+        /////////////////////////////////////
+
+        /////////////////////////////////////
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+        /////////////////////////////////////
+
+
+
+        /////////////////////////////////////
+        float mouseX = Input.GetAxis("Mouse Y") * -1 * mouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse X") * mouseSensitivity;
+
+        playerTargetRotation *= Quaternion.Euler(0, mouseY, 0);
+        cameraTargetRotation *= Quaternion.Euler(mouseX, 0, 0);
+
+        cameraTargetRotation = ClampRotationAroundXAxis(cameraTargetRotation);
+
+        transform.localRotation = playerTargetRotation;
+        cam.localRotation = cameraTargetRotation;
+        /////////////////////////////////////
+    }
+
+    void GroundCheck()
+    {
+        RaycastHit hitInfo;
+
+        if (Physics.SphereCast(transform.position, GetComponent<CapsuleCollider>().radius, Vector3.down, out hitInfo, 1.02f, Physics.AllLayers, QueryTriggerInteraction.Ignore))
+        {
+            isGrounded = true;
+            rb.drag = 5;
+        }
+        else
+        {
+            isGrounded = false;
+            rb.drag = 2;
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(cam.position, cam.forward, out hit, range))
+        {
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+            //GameObject impactGo = Instantiate()
+        }
+    }
+
+    Quaternion ClampRotationAroundXAxis(Quaternion q)
+    {
+        q.x /= q.w;
+        q.y /= q.w;
+        q.z /= q.w;
+        q.w = 1f;
+
+        float angleX = 2f * Mathf.Rad2Deg * Mathf.Atan(q.x);
+
+        angleX = Mathf.Clamp(angleX, -80f, 80f);
+
+        q.x = Mathf.Tan(0.5f * Mathf.Deg2Rad * angleX);
+
+        return q;
+    }
+}
--- Shatterspace/Assets/Scripts/PlayerController.cs
+++ Shatterspace/Assets/Scripts/PlayerController.cs
@@ -11,13 +11,30 @@ public class PlayerController : MonoBehaviour {
     [SerializeField] private float maxCamHeight = 100;
     [SerializeField] private float minCamHeight = 20;
     [SerializeField] private float zoomSensitivity = 10;
+    [SerializeField] private float horizontalRotationSpeed = 1;
+    [SerializeField] private float verticalRotationSpeed = 1;
+    [SerializeField] private float minVerticalRotation = 90;
+    [SerializeField] private float maxVerticalRotation = 10;
+
+    public Vector3 startingRotation;
 
     public List<GameObject> squads;
+
+    private GameManager _gameManager;
+
+
+    private Quaternion camTargetRotation;
 
 	void Start () {
         _rb = GetComponent<Rigidbody>();
 
+        startingRotation = new Vector3(90, 0, 0);
+
         CheckForSquads();
+
+        _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+        camTargetRotation = transform.rotation;
 	}
 	
 	void Update () {
@@ -31,6 +48,38 @@ public class PlayerController : MonoBehaviour {
         float zoomPos = transform.position.y - Input.GetAxis("Mouse ScrollWheel") * zoomSensitivity;
         zoomPos = Mathf.Clamp(zoomPos, minCamHeight, maxCamHeight);
         transform.position = new Vector3(0, zoomPos, 0);
+
+        //int rotHorizontal = 0;
+        //if (Input.GetKey(KeyCode.LeftArrow))
+        //{
+        //    rotHorizontal++;
+        //}
+        //if (Input.GetKey(KeyCode.RightArrow))
+        //{
+        //    rotHorizontal--;
+        //}
+        //_rb.rotation = Quaternion.AngleAxis(transform.rotation.y + rotHorizontal * horizontalRotationSpeed, Vector3.up);
+
+        int rotVertical = 0;
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rotVertical++;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rotVertical--;
+        }
+
+        camTargetRotation *= Quaternion.Euler(-rotVertical * verticalRotationSpeed, 0, 0);
+
+        camTargetRotation = _gameManager.ClampRotationAroundXAxis(camTargetRotation, minVerticalRotation, maxVerticalRotation);
+
+        transform.rotation = camTargetRotation;
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            transform.rotation = Quaternion.Euler(startingRotation.x, startingRotation.y, startingRotation.z);
+        }
     }
 
     public void CheckForSquads()
