77de79cf8949803f7eabc82ff370285e3907a411
ccegeadamcc <egemenkazanasmaz@gmail.com>
2018-08-05 18:09:53 +0300

    Hotfix

---
+++ Shatterspace/Assets/Scripts/FogOfWarExplorer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogOfWarExplorer : MonoBehaviour {
+
+    [SerializeField] private GameObject exploredAreaAbsorber;
+
+    bool explored;
+
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("UpdateStatus", 0f, 0.8f);
+	}
+	
+	// Update is called once per frame
+	void UpdateStatus () {
+        explored = false;
+        foreach (var collider in Physics.OverlapSphere(transform.position, (GetComponent<CapsuleCollider>().radius * 1.10f)))
+        {
+            if (collider.gameObject.tag == "FogExplored") {
+                explored = true;
+                break;
+            }
+        }
+
+        if (!explored) {
+            GameObject tempObjectVariable;
+            tempObjectVariable = Instantiate(exploredAreaAbsorber, transform.position, Quaternion.identity);
+            tempObjectVariable.GetComponent<CapsuleCollider>().radius = gameObject.GetComponent<CapsuleCollider>().radius;
+            tempObjectVariable.GetComponent<CapsuleCollider>().height = gameObject.GetComponent<CapsuleCollider>().height;
+            tempObjectVariable.GetComponent<CapsuleCollider>().contactOffset = gameObject.GetComponent<CapsuleCollider>().contactOffset;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        GameObject tempObjectVariable;
+        tempObjectVariable = Instantiate(exploredAreaAbsorber, transform.position, Quaternion.identity);
+        tempObjectVariable.GetComponent<CapsuleCollider>().radius = gameObject.GetComponent<CapsuleCollider>().radius;
+        tempObjectVariable.GetComponent<CapsuleCollider>().height = gameObject.GetComponent<CapsuleCollider>().height;
+        tempObjectVariable.GetComponent<CapsuleCollider>().contactOffset = gameObject.GetComponent<CapsuleCollider>().contactOffset;
+    }
+}
